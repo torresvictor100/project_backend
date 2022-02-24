b82ee27bdfac821d3ae1782eedea9b6ee31ad2b9
Victor Torres <torresvictor100@gmail.com>
2022-02-24 20:00:06 -0300

    Inicio do backend do projeto

---
+++ src/main/java/com/allocation/backend/projeto/backend/projeto/BackendProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.allocation.backend.projeto.backend.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allocation/backend/projeto/backend/projeto/BackendProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allocation.backend.projeto.backend.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
