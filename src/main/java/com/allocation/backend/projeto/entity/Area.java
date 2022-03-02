66cbc355f9bc9a7722561080341497a90071d116
Joao Fontes <joaoarnaldo1@hotmail.com>
2022-03-02 20:28:49 -0300

    Alterações na classe Area enum, allocation controller e professor service.

--- src/main/java/com/allocation/backend/projeto/entity/Area.java
+++ src/main/java/com/allocation/backend/projeto/entity/Area.java
@@ -1,11 +1,21 @@
 package com.allocation.backend.projeto.entity;
 
 public enum Area {
-	BIOLOGICAS,
-	EXATAS,
-	HUMANAS;
-	
-	
+	BIOLOGICAS(1), EXATAS(2), HUMANAS(3);
 
+	private int valorArea;
+
+	Area(int valor) {
+		valorArea = valor;
+	}
+
+	public int getValorArea() {
+		return valorArea;
+	}
+
+	public void setValorArea(int valorArea) {
+		this.valorArea = valorArea;
+	}
+	
+	
 }
-
--- src/main/java/com/allocation/backend/projeto/service/ProfessorService.java
+++ src/main/java/com/allocation/backend/projeto/service/ProfessorService.java
@@ -89,7 +89,7 @@ public class ProfessorService {
 	}
 	
 	public void teste() {
-		//..//
+		
 	}
 	
 	
