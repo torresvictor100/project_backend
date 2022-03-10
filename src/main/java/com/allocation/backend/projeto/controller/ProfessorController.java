04d99df3a09e209922f98e33479deb677ff9cdac
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-10 20:08:03 -0300

    Add findByName e findByCpf in ProfessorControll

--- src/main/java/com/allocation/backend/projeto/controller/ProfessorController.java
+++ src/main/java/com/allocation/backend/projeto/controller/ProfessorController.java
@@ -57,6 +57,34 @@ public class ProfessorController {
 		}
 	}
 	
+	@ApiOperation(value = "Find a professor by name")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found") })
+	@GetMapping(path = "/name/{professor_name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Professor> findByName(@PathVariable(name = "department_name") String name) {
+		Professor professor = professorService.findByName(name);
+		if (professor == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(professor, HttpStatus.OK);
+		}
+	}
+	
+	@ApiOperation(value = "Find a professor by cpf")
+	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 404, message = "Not Found") })
+	@GetMapping(path = "/cpf/name/{professor_cpf}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Professor> findByCpf(@PathVariable(name = "department_cpf") String cpf) {
+		Professor professor = professorService.findByCpf(cpf);
+		if (professor == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(professor, HttpStatus.OK);
+		}
+	}
+	
 	@ApiOperation(value = "Find professor by department")
 	@ApiResponses({ @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 400, message = "Bad Request") })
 	@GetMapping(path = "department/{professor_id}", produces = MediaType.APPLICATION_JSON_VALUE)
